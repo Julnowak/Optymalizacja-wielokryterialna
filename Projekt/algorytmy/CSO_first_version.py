1c178a9663ba0d1448be0183c554e31d0aeb56ae
AdamZlocki <zlocki@student.agh.edu.pl>
2025-01-12 23:53:13 +0100

    maybe (XD) fixed dispersal and reformat

--- Projekt/algorytmy/CSO_first_version.py
+++ Projekt/algorytmy/CSO_first_version.py
@@ -4,7 +4,8 @@ import time
 
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from terrain import terrain_generator
 
@@ -14,12 +15,17 @@ def plot_graph(best_path):
     plt.figure(figsize=(5, 5))
     # plt.grid("off")
     # plt.rc("axes", axisbelow=True)
-    plt.imshow(terrain,)
+    plt.imshow(
+        terrain,
+    )
 
     X = [best_path[i][0] for i in range(len(best_path))]
     Y = [best_path[i][1] for i in range(len(best_path))]
-    plt.plot(X, Y, "ro-",)
-
+    plt.plot(
+        X,
+        Y,
+        "ro-",
+    )
 
     for i in range(len(best_path)):
         plt.scatter(
@@ -41,6 +47,7 @@ def plot_graph(best_path):
 #         cost += ter[p[0]][p[1]] * 100
 #     return cost
 
+
 def loss_function(path, ter):
     cost = 0
     num = 0
@@ -48,7 +55,7 @@ def loss_function(path, ter):
     p_last = None
     for p in path:
         if num != 0:
-            dist_penalty = np.sqrt((p[0] - p_last[0])**2 + (p[1] - p_last[1])**2)
+            dist_penalty = np.sqrt((p[0] - p_last[0]) ** 2 + (p[1] - p_last[1]) ** 2)
         p_last = p
         cost += ter[p[0]][p[1]] * 100 + dist_penalty * 200000
     return cost
@@ -56,10 +63,16 @@ def loss_function(path, ter):
 
 def calculate_neighbourhood(point, map_size):
     neigh = []
-    for i in [-1,0,1]:
-        for j in [-1,0,1]:
-            p = [point[0] + i, point[1]+j]
-            if p[0] >= 0 and p[1] >= 0 and p[0] <= map_size[0] and p[1] <= map_size[1] and p != point:
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            p = [point[0] + i, point[1] + j]
+            if (
+                p[0] >= 0
+                and p[1] >= 0
+                and p[0] <= map_size[0]
+                and p[1] <= map_size[1]
+                and p != point
+            ):
                 neigh.append(p)
     return neigh
 
@@ -76,8 +89,12 @@ def initial_path(start, end, map_size):
         neighbours = calculate_neighbourhood(current, map_size)
         # Filter neighbours to ensure movement towards the end point
         valid_neighbours = [
-            n for n in neighbours
-            if n[0] >= current[0] and n[1] >= current[1] and n[0] <= end[0] and n[1] <= end[1]
+            n
+            for n in neighbours
+            if n[0] >= current[0]
+            and n[1] >= current[1]
+            and n[0] <= end[0]
+            and n[1] <= end[1]
         ]
 
         # Randomly select the next point from valid neighbours
@@ -88,6 +105,7 @@ def initial_path(start, end, map_size):
                 path.append(current)
     path.append(end)
     return path
+
 
 def count_fits(large):
     counter = 0
@@ -127,12 +145,12 @@ def step_distance(p1, p2):
                 n = [np.abs(p1[i][0] - 0), np.abs(p1[i][1] - 0)]
             suma += count_fits(n)
     else:
-
         for i in range(len(p1)):
             n = [np.abs(p1[i][0] - p2[i][0]), np.abs(p1[i][1] - p2[i][1])]
             suma += count_fits(n)
 
     return suma
+
 
 def cso_step(actual, best):
     new_actual = actual.copy()
@@ -170,12 +188,15 @@ def cso_step(actual, best):
 
 def dispersal(actual, map_size):
     new_actual = actual.copy()
-    x = random.randint(1,10)
-    i = random.randint(0, len(actual)-1)
+    x = random.randint(1, 10)
+    i = random.randint(0, len(actual) - 1)
     if x == 1:
         new = new_actual[0]
         while new in new_actual:
-            new = [random.randint(1,map_size[0]-1), random.randint(1,map_size[1]-1)]
+            new = [
+                random.randint(1, map_size[0] - 1),
+                random.randint(1, map_size[1] - 1),
+            ]
         new_actual.append(new)
     elif x == 2:
         if len(new_actual) > 1:
@@ -183,30 +204,36 @@ def dispersal(actual, map_size):
     else:
         addon = 1
         if x == 3:
-            new_actual[i][0] += addon
-            new_actual[i][1] += addon
+            new_actual[i][0] += addon if new_actual[i][0] < map_size[0] else 0
+            new_actual[i][1] += addon if new_actual[i][1] < map_size[1] else 0
         elif x == 4:
-            new_actual[i][0] += addon
+            new_actual[i][0] += addon if new_actual[i][0] < map_size[0] else 0
         elif x == 5:
-            new_actual[i][0] += addon
-            new_actual[i][0] -= addon
+            new_actual[i][0] += addon if new_actual[i][0] < map_size[0] else 0
+            new_actual[i][1] -= addon if new_actual[i][1] > 0 else 0
         elif x == 6:
-            new_actual[i][1] += addon
+            new_actual[i][1] += addon if new_actual[i][1] < map_size[1] else 0
         elif x == 7:
-            new_actual[i][1] -= addon
+            new_actual[i][1] -= addon if new_actual[i][1] > 0 else 0
         elif x == 8:
-            new_actual[i][0] -= addon
-            new_actual[i][1] -= addon
+            new_actual[i][0] -= addon if new_actual[i][0] > 0 else 0
+            new_actual[i][1] -= addon if new_actual[i][1] > 0 else 0
         elif x == 9:
-            new_actual[i][0] -= addon
+            new_actual[i][0] -= addon if new_actual[i][0] > 0 else 0
         elif x == 10:
-            new_actual[i][0] -= addon
-            new_actual[i][0] += addon
+            new_actual[i][0] -= addon if new_actual[i][0] > 0 else 0
+            new_actual[i][1] += addon if new_actual[i][1] < map_size[1] else 0
 
     for i in new_actual:
-        if i[0] > map_size[0] or i[1] > map_size[1] or i[0] < map_size[0] or i[1] < map_size[1]:
+        if (
+            i[0] > map_size[0]
+            or i[1] > map_size[1]
+            or i[0] < map_size[0]
+            or i[1] < map_size[1]
+        ):
             return actual
     return new_actual
+
 
 class Solution:
     def __init__(self, path, loss_value):
@@ -214,9 +241,17 @@ class Solution:
         self.loss_value = loss_value
 
 
-def algorithm(start, end, map_size, terrain, visibility_range, num_of_iterations: int = 100, cockroaches_num: int = 20,
-              probability_of_dispersion: float = 10, max_step: int = 1, ):
-
+def algorithm(
+    start,
+    end,
+    map_size,
+    terrain,
+    visibility_range,
+    num_of_iterations: int = 100,
+    cockroaches_num: int = 20,
+    probability_of_dispersion: float = 10,
+    max_step: int = 1,
+):
     solutions = []
     best_solution = Solution(None, np.inf)  # path, cost,
     for i in range(cockroaches_num):
@@ -234,15 +269,19 @@ def algorithm(start, end, map_size, terrain, visibility_range, num_of_iterations
 
     N = len(solutions)
     for _ in range(num_of_iterations):
-
         # 2 - Znalezienie minimum lokalnego i globalnego
         for sol_i in range(N):
             pi = solutions[sol_i]
             for sol_j in range(N):
-                if 0 < step_distance(solutions[sol_i].path, solutions[sol_j].path) <= visibility_range and solutions[sol_j].loss_value < solutions[sol_i].loss_value:
+                if (
+                    0
+                    < step_distance(solutions[sol_i].path, solutions[sol_j].path)
+                    <= visibility_range
+                    and solutions[sol_j].loss_value < solutions[sol_i].loss_value
+                ):
                     pi = solutions[sol_j]
 
-        # 3 - Implementacja ruchu roju oraz ponowne uaktualnienie pg
+            # 3 - Implementacja ruchu roju oraz ponowne uaktualnienie pg
             if pi is solutions[sol_i]:
                 for _ in range(max_step):
                     solutions[sol_i].path = cso_step(solutions[sol_i].path, pg.path)
