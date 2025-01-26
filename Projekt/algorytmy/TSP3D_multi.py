24d9c253d2a74adf8b65f275c61156ef3df5e52e
Julnowak <julnowak@student.agh.edu.pl>
2025-01-26 14:48:21 +0100

    Update TSP3D_multi.py

--- Projekt/algorytmy/TSP3D_multi.py
+++ Projekt/algorytmy/TSP3D_multi.py
@@ -381,12 +381,15 @@ def run_tsp3d_path_single_robot(
     Zwraca najlepszą znalezioną ścieżkę (listę (r,c)) i jej koszt.
     """
     # t0 = time.time()
+    best_cost_list = []
 
     # 1) Inicjalizacja populacji
     population = ga.generate_population(pop_size, tuple(start_2d), tuple(end_2d))
 
     best = min(population, key=lambda p: ga.distance_path(p))
     best_cost = ga.distance_path(best)
+
+    best_cost_list.append(best_cost)
     # init_time = time.time() - t0
     # print(f"Initial best dist={best_cost:.4f} | init_time={init_time:.2f}s")
 
@@ -427,16 +430,17 @@ def run_tsp3d_path_single_robot(
         # f) aktualizacja best
         curr = min(population, key=lambda p: ga.distance_path(p))
         curr_cost = ga.distance_path(curr)
+
         if curr_cost < best_cost:
             best = curr
             best_cost = curr_cost
-
+        best_cost_list.append(best_cost)
         gen_time = time.time() - gen_start
         print(f"GEN {gen+1}/{num_of_generations} - best so far={best_cost:.4f}, time={gen_time:.2f} sec")
 
     total_end = time.time()
     print(f"\nCalkowity czas GA (dla 1 robota): {(total_end - total_start):.2f} sec.")
-    return best, best_cost
+    return best, best_cost_list
 
 
 def run_multi_robot_tsp3d_path(
@@ -460,6 +464,7 @@ def run_multi_robot_tsp3d_path(
     ga = GeneticTSP3DPath(terrain, occupied_paths=[])
 
     all_paths = []
+    best_cost_lists = []
     for i, (st, en) in enumerate(zip(starts_2d, ends_2d)):
         print(f"\n=== Planowanie dla robota {i+1}: start={st}, end={en} ===")
         best_path, best_val = run_tsp3d_path_single_robot(
@@ -472,15 +477,17 @@ def run_multi_robot_tsp3d_path(
             offspring_rate=offspring_rate,
             mutation_rate=mutation_rate
         )
-        print(f"Robot {i+1} - best cost={best_val:.4f}")
+        # print(f"Robot {i+1} - best cost={best_val:.4f}")
         print(f"Best path={best_path}\n")
+
+        best_cost_lists.append(best_val)
 
         # Dodaj tę ścieżkę do occupied_paths,
         # aby kolejne roboty unikały kolizji.
         ga.occupied_paths.append(best_path)
         all_paths.append(best_path)
 
-    return all_paths
+    return all_paths, best_cost_lists
 
 #############################
 #  4) MAIN DEMO             #
@@ -491,6 +498,8 @@ if __name__ == "__main__":
     # 4 roboty, każdy ma wspólny cel
     starts = [(5, 10), (20, 0), (0, 30), (1, 1)]
     ends   = [(48, 49), (48, 49), (48, 49), (48, 49)]
+    starts = [(5, 10),]
+    ends   = [(48, 49),]
 
     # Teren 51x51 (jak w A*)
     map_size = (51, 51)
@@ -501,7 +510,7 @@ if __name__ == "__main__":
     )
 
     # Parametry GA
-    pop_size = 2500
+    pop_size = 1000
     num_of_generations = 50
     elite_size = 5
     tournament_size = 3
@@ -509,7 +518,7 @@ if __name__ == "__main__":
     mutation_rate = 0.8
 
     # Uruchamiamy wielorobotowe planowanie
-    final_paths = run_multi_robot_tsp3d_path(
+    final_paths, costs = run_multi_robot_tsp3d_path(
         terrain,
         starts,
         ends,
@@ -526,3 +535,8 @@ if __name__ == "__main__":
 
     # Wizualizacja animowana (krok-po-kroku)
     plot_graphs_animation(final_paths, terrain, starts, ends)
+
+    for c in costs:
+        plt.plot(c)
+
+    plt.show()
